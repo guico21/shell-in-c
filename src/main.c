05eea8c299e5fec391ce75173a573105a6c6c3da
guico21 <156781946+guico21@users.noreply.github.com>
2026-03-05 10:15:55 +0000

    Add 'cd' command and improve input parsing

--- src/main.c
+++ src/main.c
@@ -4,8 +4,92 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <ctype.h>
 
-const char *builtin_cmds [] = {"exit", "echo", "type", "pwd"}; // array of pointers to litterals
+#define BUFFER 16
+
+const char *builtin_cmds [] = {"exit", "echo", "type", "pwd", "cd"}; // array of pointers to litterals
+
+static void free_argv(char **argv, int argc) {
+    if (!argv) return;
+    for (int i = 0; argv[i] != NULL; i++) {
+        free(argv[i]);
+        argv[i] = NULL;
+    }
+}
+
+int parse_user_input(const char *input, char **argv, size_t argc_cap) {
+  size_t argc = 0;
+  size_t i = 0;
+  int in_squote = 0;
+  int in_dquote = 0;
+
+  if (!input || !argv || argc_cap == 0) return 0;
+  while (input[i] != '\0') {
+    while (isspace((unsigned char)input[i])) i++; // we do unsigned casting cause isspace does not like signed char
+    if (input[i] == '\0') break;
+    if (argc + 1 >= argc_cap) break;  // keep space for final NULL
+
+    size_t start = i;
+    size_t out_len = 0;
+
+    // First pass: find end of token and compute output length excluding quotes
+    in_squote = 0;
+    in_dquote = 0;
+    while (input[i] != '\0') {
+      unsigned char c = (unsigned char)input[i];
+      if (!in_squote && !in_dquote && isspace(c)) break;
+      if (c == '\'' && !in_dquote) {
+        in_squote = !in_squote;
+        i++;
+        continue;
+      }
+      if (c == '\"' && !in_squote){
+        in_dquote = !in_dquote;
+        i++;
+        continue;
+      }
+      out_len++;
+      i++;
+    }
+    if (in_squote || in_dquote) {  // quote not closed before token ended
+      free_argv(argv, (int)argc);
+      return -1;
+    }
+    char *tok = (char *)malloc(out_len + 1);
+    if (!tok) {
+      free_argv(argv, (int)argc);
+      return -2;
+    }
+
+    // Second pass: copy chars excluding quotes
+    size_t r = start;
+    size_t w = 0;
+    in_squote = 0;
+    in_dquote = 0;
+    while (w < out_len && input[r] != '\0') {
+      unsigned char c = (unsigned char)input[r];
+      if (!in_squote && !in_dquote && isspace(c)) break;
+      if (c == '\'' && !in_dquote) {
+        in_squote = !in_squote;
+        r++;
+        continue;
+      }
+      if (c == '\"' && !in_squote){
+        in_dquote = !in_dquote;
+        r++;
+        continue;
+      }
+      tok[w++] = (char)c;
+      r++;
+    }
+    tok[w] = '\0';
+    argv[argc++] = tok;
+    // i is already at whitespace or '\0'. Next loop will skip spaces.
+  }
+  argv[argc] = NULL;
+  return (int)argc;
+}
 
 size_t is_builtin_cmd(char *input){
   size_t n = sizeof(builtin_cmds) / sizeof(builtin_cmds[0]);
@@ -13,13 +97,6 @@ size_t is_builtin_cmd(char *input){
     if (strcmp(input,builtin_cmds[aux]) ==0 ) return 1;
   }
   return 0;
-}
-
-size_t check_command(char *input, char *command) {
-  // Check if the input starts with the command followed by a space
-  int command_len = strlen(command);
-  return strncmp(input, command, command_len) == 0 &&
-         (input[command_len] == ' ' || input[command_len] == '\0');
 }
 
 int find_in_path(const char *command, const char *path_env, char *out, size_t out_len){
@@ -69,107 +146,117 @@ int find_in_path(const char *command, const char *path_env, char *out, size_t ou
   return 0;
 }
 
-size_t create_args_from_string(char *buf, const char *delims, char **argv, size_t argv_cap ){
-  size_t argc = 0;
-  char *save = NULL;
-
-  char *tok = strtok_r(buf, delims, &save);
-  while (tok != NULL){
-    if (argc + 1 >= argv_cap) {
-        break;
-    }
-    argv[argc] = tok;
-    argc = argc + 1;
-    // Pass NULL to continue from where it left off
-    tok = strtok_r(NULL, delims, &save);
-  }
-  argv[argc] = NULL;
-  return argc;
-}
-
-
-
 int main(){
-  char user_input[50];
-  char temp[50];
-  const char *path = "PATH";           // This is the PATH varibale we want to look into the system
-  int command_found = 0;
-  char *command = NULL;
-  char *path_command = NULL;
+  char *user_input = NULL;
+  size_t line_cap = 0;
   char candidate[4096];
-  char *argv[16]; // This is for the arguments
+  char *argv[BUFFER]; // This is for the arguments
 
   // Flush after every printf
   setbuf(stdout, NULL);
-  const char *path_env = getenv(path);
+  const char *path_env = getenv("PATH");
   int path_exist = (path_env != NULL);
-
   if (!path_exist){
     printf("PATH does not exist or is corrupted.\n");
   }
-
   while (1){
-    command_found = 0;
     printf("$ ");
+    ssize_t nread = getline(&user_input, &line_cap, stdin); // POSIX, hence ok for macOS and Linux. Hides several calls for memory management
     // The function below allows a full string to be read, including the ending caracter '\n'
-    if (fgets(user_input, sizeof(user_input), stdin) == NULL){
-      printf("Null entry. Exiting...\n");
-      return 0;
+    if (nread < 0){
+      printf("\n");
+      break; //<-- #TODO: this might need to be continue
     }
 
     // With the below function, what we are doing is calculating the size of the string,
     // stopping at the character '\n'
     user_input[strcspn(user_input, "\n")] = '\0';
     // Isolating the first part of the string, which is meant to be a command
-
-    strncpy(temp, user_input, sizeof(user_input));
-    temp[sizeof(temp) - 1] = '\0';
-    command = strtok(temp, " ");
-    
-    if ( !command_found && (command != NULL) && check_command(command, "exit")){
-      return 0;
+    int argc = parse_user_input(user_input, argv, BUFFER);
+    if (argc == -1){
+      printf("Sintax error: unterminated single quote\n");
+      free_argv(argv, BUFFER);
+      continue;
+    }
+    if (argc == 0) {
+      free_argv(argv, BUFFER);
+      continue;
+    }
+    if (argv[0] == NULL) {
+      continue;
+    }
+    char *command = argv[0];
+    if (strcmp(command, "exit") == 0){
+      free_argv(argv, BUFFER);
+      break;
     }
 
-    if ( !command_found && (command != NULL) && check_command(command, "echo")){
-      command = strtok(NULL, " ");
-      while(command) { 
-        printf("%s ",command);
-        command = strtok(NULL, " ");
+    if (strcmp(command, "echo") == 0){
+      for (size_t i = 1; i < argc; i++){
+        if (i > 1) printf(" ");
+        printf ("%s", argv[i]);
       }
-      command_found = 1;
       printf("\n");
+      free_argv(argv, BUFFER);
+      continue;
     }
 
-    if (!command_found && (command != NULL) && check_command(command, "pwd")){
-      printf("%s\n", getcwd(NULL, 0)); // Buffer and size automatically managed by UNIX
-      command_found = 1;
+    if (strcmp(command, "pwd") == 0){
+      char *cwd = getcwd(NULL, 0); // Buffer and size automatically managed by UNIX
+      // In this case we stored into cwd for later freeing up memory, cause internally
+      // getcwd uses malloc. if we do not track it with a pointer, we lose track of memory
+      if (!cwd){
+        perror("getcwd");
+        free_argv(argv, BUFFER);
+        continue;
+      }
+      printf("%s\n", cwd);
+      free(cwd);
+      free_argv(argv, BUFFER);
+      continue;
     }
 
-    if(!command_found && (command != NULL) && check_command(command, "type")){
-      command = strtok(NULL, " ");
-      if (command ==NULL){
+    if (strcmp(command, "cd") == 0){
+      const char *dest = NULL;
+      if (argc >= 2){
+        dest = argv[1];
+        if (strcmp(dest, "~") == 0){
+          dest = getenv("HOME");
+        }
+      }else{
+        dest = getenv("HOME");
+      }
+
+      if (!dest) {
+        printf("cd: HOME not set\n");
+        continue;
+      }
+      if (chdir(dest) != 0){
+        printf("cd: %s: No such file or directory\n",dest);
+      }
+      continue;
+    }
+
+    if(strcmp(command, "type") == 0){
+      if (argc < 2){
         printf("No command has been inserted.\n");
         continue;
       }
-      if (is_builtin_cmd(command)){
-        printf("%s is a shell builtin\n", command);
-        command_found = 1;
-      } 
-      else {
-        if (path_exist && find_in_path(command, path_env, candidate, sizeof(candidate))) {
-          printf("%s is %s\n", command, candidate);
-          command_found = 1;
-        } 
-        else {
-          printf("%s: not found\n", command);
-          command_found = 1;
-        }
+      char *q = argv[1];
+      if (is_builtin_cmd(q)){
+        printf("%s is a shell builtin\n", q);
+      } else if (path_exist && find_in_path(q, path_env, candidate, sizeof(candidate))) {
+          printf("%s is %s\n", q, candidate);
+      } else {
+          printf("%s: not found\n", q);
       }
+      free_argv(argv, BUFFER);
+      continue;
     }
-    else if (!command_found && path_exist && find_in_path(command, path_env, candidate, sizeof(candidate))){
+
+    if (path_exist && find_in_path(command, path_env, candidate, sizeof(candidate))){
       // Executing the file
       // printf("%s is an executable!\n",command);
-      size_t argc = create_args_from_string(user_input, " \t\r\n", argv, 16);
 
       // I feel this section should be improved for better error handling and less bespoke code
       pid_t pid = fork(); // gets child PID
@@ -180,14 +267,14 @@ int main(){
       } else if (pid > 0) { // We are in the parent
           int status;
           waitpid(pid, &status, 0);
-          command_found = 1;
       } else {
           perror("Fork did not work out.");
       }
+      free_argv(argv, BUFFER);
+      continue;
     }
-    if (!command_found){
-      printf("%s: command not found\n", command);
-    }
+    printf("%s: command not found\n", command);
   }
+  free(user_input);
   return 0;
 }
